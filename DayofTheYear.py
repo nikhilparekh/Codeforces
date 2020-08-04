a617b6e24d5e75570d98272cdf8d8fd49a2f215c
Nikhil Parekh <nksp777@gmail.com>
2020-08-04 01:42:15 -0700

    Day of the year

--- DayofTheYear.py
+++ DayofTheYear.py
@@ -1,9 +1,14 @@
-  yr = int(date[0:4])
+def dayOfYear(date: str):
+    yr = int(date[0:4])
     mn = int(date[5:7])
     day = int(date[8:])
     days = [31,28,31,30,31,30,31,31,30,31,30,31]
     if(yr%400==0 or ((yr%4==0)and (yr%100!=0))):
         if(mn>2):
             return sum(days[:mn-1])+day+1
+        else:
+            return sum(days[:mn-1])+day
     else:
         return sum(days[:mn-1])+day
+
+print(dayOfYear("2016-02-29"))
